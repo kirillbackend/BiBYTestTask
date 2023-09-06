5b9a9a1c6c910b2d3de77e596eb76679f212dbb6
kirill <zinchenkokir@gmail.com>
2023-09-06 11:15:18 +0300

    FirstTask

---
+++ TestTask/TestTask/Controllers/SumModuloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestTask.Services.Interface;
+
+namespace TestTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumModuloController : ControllerBase
+    {
+        [HttpGet, Route("Sum")]
+        public async Task<int> GetSumAsync([FromQuery] int[] numbers, ISum sumService)
+        {
+            return await sumService.GetSumAsync(numbers);
+        }
+    }
+}
